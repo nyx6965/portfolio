f0d26907c3e5318a6e9e2a674e5694f5aee6599c
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-12-20 20:41:13 +0300

    New

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/porfolio
 
-go 1.22.6
+go 1.23.0
 
 require (
-	github.com/a-h/templ v0.2.778
+	github.com/a-h/templ v0.3.960
 	github.com/go-chi/chi/v5 v5.1.0
 )
--- main.go
+++ main.go
@@ -6,22 +6,18 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	view "github.com/porfolio/packages/view"
-	blogs "github.com/porfolio/packages/view/blogs"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-    r.Use(middleware.Logger)
+	r.Use(middleware.Logger)
 
-	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		component := view.Home()
 		component.Render(r.Context(), w)
 	})
-	r.Get("/compliers", func(w http.ResponseWriter, r *http.Request) {
-		component := blogs.Compliers()
-		component.Render(r.Context(), w)
-	})
+
 	http.ListenAndServe(":3000", r)
 }
